1e912c7a83da7b6adc0b3a5cb408989226d2b1ed
Yong <pyg941007@hanmail.net>
2021-06-03 13:52:05 +0900

    text 관련된 경우 메소드 추가, 속성들을 알아볼 예정

--- app/src/main/java/com/compose/myapplication/MainActivity.kt
+++ app/src/main/java/com/compose/myapplication/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.compose.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,6 +42,38 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        // Greeting("Android") // Text
+        // MultipleColumnText() // 열로 정렬된 text
+        // MultipleRowText() // 행으로 정렬된 text
+        // ColumnWithRowText() // 행과 열로 정렬된 text
+    }
+}
+
+@Composable
+fun MultipleColumnText(){
+    Column(Modifier.padding(5.dp)) {
+        Text(text = "Hello")
+        Text(text = "This text is column")
+        Text(text = "sorted")
+    }
+}
+
+@Composable
+fun MultipleRowText(){
+    Row(Modifier.padding(5.dp)) {
+        Text(text = "Hello")
+        Text(text = "This text is column")
+        Text(text = "sorted")
+    }
+}
+
+@Composable
+fun ColumnWithRowText(){
+    Row(Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Hello, MyName is ")
+        Column(Modifier.padding(5.dp)) {
+            Text(text = "Ura")
+            Text(text = "Janis")
+        }
     }
 }
